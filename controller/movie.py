0f8612872cffb7371a4a447cd2e3f144c74db0ee
Nicolas <nik245olas@gmail.com>
2022-11-18 11:59:12 -0500

    get funcionando usando diccionarios

--- controller/movie.py
+++ controller/movie.py
@@ -1,18 +1,33 @@
 from application import app
 from model.repository import MovieRepository
 from model.entity import Movie
+from flask import request
 
 repository = MovieRepository
 
 @app.route("/api/movies", methods=["GET"])
 def list():
-    return repository.findAll()
+    movies = repository.findAll()
+    response = []
+    for movie in movies:
+        response.append(movie.toDic())
+    
+    return response, 200
 
 @app.route("/api/movies/<code>", methods = ["GET"])
 def findByCode(code):
-    return repository.findByCode(code)
+    movie = repository.findByCode(code)
+    return movie.toDic(), 200
+
 
 @app.route("/api/movies", methods= ["POST"])
 def create():
-    movie= Movie()
+    code = request.json["code"]
+    name = request.json["name"]
+    image = request.json["image_url"]
+    year = request.json["year"]
+    
+    movie= Movie(code, name, image_url= image, year = year)
     repository.insert(movie)
+    
+    return "{ 'message': 'Muy bien' }", 201
